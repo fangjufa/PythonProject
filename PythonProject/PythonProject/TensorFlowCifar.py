ad33ce9fc67f60bb4d40fc83d9c546677e47c710
fangjufa <934363382@qq.com>
2017-11-07 23:38:56 +0800

    2017110702
    调试问题修改。

--- PythonProject/PythonProject/TensorFlowCifar.py
+++ PythonProject/PythonProject/TensorFlowCifar.py
@@ -23,8 +23,12 @@ def ReadDatasets(file):
 #num = input.shape[0]
 
 def Weight_Initial(shape):
-    weight = tf.truncated_normal(shape=shape)
+    weight = tf.truncated_normal(shape=shape,stddev = 0.1)
     return tf.Variable(weight)
+
+def Bias_Initial(shape):
+    bias = tf.constant(0.1,shape=shape)
+    return tf.Variable(bias)
 
 #input = tf.cast(tf.Variable(train_input),tf.float32)
 ##[10000,32,32,3]
@@ -43,7 +47,7 @@ tlbls = tf.placeholder(dtype=tf.float32)
 W1 = Weight_Initial([5,5,3,32])
 W2 = Weight_Initial([5,5,32,64])
 W3 = Weight_Initial([1600,10])
-b = Weight_Initial([10])
+b = Bias_Initial([10])
 
 #first convolution
 #[10000,28,28,32]
@@ -64,100 +68,34 @@ fc = tf.reshape(maxpool2,shape=[-1,1600])
 scores = tf.matmul(fc,W3) + b
 #计算的y值，然后通过交叉熵，求loss，通过最小化loss求梯度。
 #[10000,10]
-y = tf.nn.softmax(scores)
+y = tf.nn.softmax(scores,dim=0)
 
 #交叉熵计算公式
 loss = -tf.reduce_sum(tlbls*tf.log(y))
 
 adam = tf.train.AdamOptimizer(0.001)
 minimize = adam.minimize(loss)
-#grad_b = adam.compute_gradients(loss,b)
 
 out = tf.cast(tf.argmax(y,1),dtype=tf.float32)
 #correct = tf.cast(tf.equal(tf.Variable(train_lbls),tf.cast(out,tf.int32)),tf.float32)
 correct = tf.cast(tf.equal(train_lbls,out),tf.float32)
 accuracy = tf.reduce_mean(correct)
-##predictable
-#te_input = tf.cast(tf.Variable(test_input),tf.float32)
-##[10000,32,32,3]
-#te_input = tf.reshape(te_input,shape=[10000,32,32,3])
-#te_lbls = np.zeros(shape=[10000,10])
-#for i in range(len(test_lbls)):
-#    te_lbls[i][test_lbls[i]] = 1
-#te_lbls = tf.Variable(te_lbls)
-
-#test_out, = ConvLayers(te_input,te_lbls,W1,W2,W3,b)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    #b_val = sess.run(b)
-    #print(b_val)
     for i in range(5):
         traindata = ReadDatasets("Images/cifar-10-batches-py/data_batch_"+(str)(i+1))
         #print(traindata.size)
-        for j in range(200):
+        for j in range(200):            
             tlbls_data = np.zeros([50,10])
             trainlabels = traindata.target[j*50:(j+1)*50]
-            for i in range(50):
-                tlbls_data[i][trainlabels[i]] = 1.0
+            for k in range(50):
+                tlbls_data[k][trainlabels[k]] = 1.0
             #tlbls = tf.cast(tf.Variable(tlbls),tf.float32)
-            sess.run(minimize,feed_dict={data_input:traindata.data[j*50:(j+1)*50],train_lbls:trainlabels,tlbls:tlbls_data})
             if j%100 == 0:
-                acc = sess.run(accuracy)
+                print(sess.run(b))
+                acc = sess.run(accuracy,feed_dict={data_input:traindata.data[j*50:(j+1)*50],train_lbls:trainlabels,tlbls:tlbls_data})
                 print(acc)
-    
+            sess.run(minimize,feed_dict={data_input:traindata.data[j*50:(j+1)*50],train_lbls:trainlabels,tlbls:tlbls_data})
 
-    
-    #sess.run(minimize)
-    #test_pre = sess.run(grad_b)
-    #print(test_pre)
-    #acc = sess.run(accuracy)
-    #print(acc)
-    
-
-
-
-##定义一个两行三列的矩阵
-#matrix1 = tf.constant([[2,1,3],[5,2,4]])
-
-##定义另一个三行两列的矩阵
-#matrix2 = tf.constant([[1,5],[1,3],[2,1]])
-
-##求两个矩阵的乘法值
-#product = tf.matmul(matrix1,matrix2)
-
-##定义一个标量的变量
-#state = tf.Variable(2,None)
-
-##计算一个矩阵与一个标量的乘积
-#mult = tf.multiply(matrix1,state)
-
-#mat_grad = tf.gradients(mult,matrix1)
-#sta_grad = tf.gradients(mult,state)
-
-
-##因为上面有变量定义，所以需要初始化变量
-#initvar = tf.global_variables_initializer()
-
-##启动会话,注意这个Session函数的大小写
-#with tf.Session() as sess:
-
-#    #设置使用特定的某个设备来执行，/gpu:0代表由第一个gpu来执行这个会话，/gpu:1等以此类推
-#    #但当我输入10000时，并没有报错，可能是当找不到指定的gpu时，它会自动使用默认的设备计算。
-#    #with tf.device("/gpu:0"):
-
-#    #先运行初始化变量op
-#    sess.run(initvar)
-#    result = sess.run(product)
-#    #当你需要获取多个op计算的结果时，调用多次run，传入你想获取的变量。
-#    result2 = sess.run(mult)
-#    print(result)
-#    print("multiply:",result2)
-#    #你也可以只调用一次run函数，获取多个值，如下所示
-#    #result3 = sess.run([product,mult])
-#    #print("result3:",result3[0],result3[1])
-
-#    grad = sess.run([mat_grad,sta_grad])
-#    print("gradient of mat:",grad[0])
-#    print("gradient of state:",grad[1])
 
